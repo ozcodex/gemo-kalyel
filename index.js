2833a2396b1b830037b18441299ce67efd2da70d
Oscar Bernal <oscar.bernal@lever.co>
2019-12-20 22:24:51 -0500

    Save player name in object

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const readline = require('readline').createInterface({
   output: process.stdout
 })
 
+const playerInfo = {
+  name : ""
+}
+
 readline.question('Kiu estas vi? ', (name) => {
   console.log('Saluton ' + name + '!')
+  playerInfo.name = name
   readline.close()
 })
