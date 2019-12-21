b398c8d2b8b1d226e60cc02c4a41d0b753513b0e
Oscar Bernal <oscar.bernal@lever.co>
2019-12-20 22:18:16 -0500

    Add readline library for input

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+readline.question('Kiu estas vi? ', (name) => {
+  console.log('Saluton ' + name + '!')
+  readline.close()
+})
