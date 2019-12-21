0f3c55213f4e861fd27583de81eec35b2c695a07
Oscar Bernal <oscar.bernal@lever.co>
2019-12-20 23:08:08 -0500

    Add comments and small improvements

--- index.js
+++ index.js
@@ -3,32 +3,36 @@ const readline = require('readline').createInterface({
   output: process.stdout,
 });
 
+// This Object carries the Player Info
 const playerInfo = {
-  name: '',
+  name: null,
   currentRoom: 'tst_0',
 };
 
+// This Object have all the available rooms
 const roomsList = {
   tst_0: {
-    type: '',
-    id: '',
-    north: '',
-    east: '',
-    south: '',
-    west: '',
+    type: null,
+    name: 'Testaĉambro Nulo',
+    north: null,
+    east: null,
+    south: null,
+    west: null,
     items: {},
   },
-}
+};
 
+// Entry Point, where everything starts
 readline.question('Kiu estas vi? ', name => {
   console.log('Saluton ' + name + '!');
   playerInfo.name = name;
-  where()
+  where();
   readline.close();
 });
 
-function where(){
-  console.log(playerInfo.name + " estas en la ĉambro " + playerInfo.currentRoom)
+// This function prints the current position of player
+function where() {
+  const name = playerInfo.name;
+  const room = roomsList[playerInfo.currentRoom];
+  console.log(name + ' estas en la ĉambro ' + room.name);
 }
-
-;
